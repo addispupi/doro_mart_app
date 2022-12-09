b03a8d3222c67380cb409815c09d8e9796befc74
Addisu <pupiatpixar@gmail.com>
2022-12-09 15:31:29 +0300

    Home Page - Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:doro_mart_app/screens/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Doro Mart',
       home: HomePage(),
     );
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Page")),
+      body: SafeArea(child: Text("Home Page"),),
     );
   }
 }
