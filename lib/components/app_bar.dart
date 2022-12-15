c119c2b7d273e6c70cd879312a3b3a40ad7f47fa
Addisu <pupiatpixar@gmail.com>
2022-12-15 20:35:09 +0300

    Tool Bar Logo - Added

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -19,24 +19,23 @@ class CustomAppBar extends StatelessWidget {
         children: [
           GestureDetector(
             // onTap: leftCallback != null ? () => leftCallback!() : null,
-            child: Container(
-              padding: EdgeInsets.all(8),
-              decoration:
-                  BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white),
-              child: Icon(leftIcon),
-            ),
+            child: _buildButtonIcon(leftIcon),
           ),
-          // Image.asset("assets/images/logos/doro_mart_logo.png", height: 80),
-          Container(
-            padding: EdgeInsets.all(8),
-            decoration:
-                BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-            child: Icon(Icons.shopping_cart),
-          ),
+          Image.asset("assets/logos/logo.png", height: 80, width: 200,),
+          _buildButtonIcon(rightIcon),
         ],
       ),
     );
+  }
+
+  Widget _buildButtonIcon(IconData icon) {
+    return Container(
+            padding: EdgeInsets.all(8),
+            decoration:
+                BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white),
+            child: Icon(icon),
+          );
   }
 }
