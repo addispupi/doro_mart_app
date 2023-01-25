8f50659ae2fe937e8812819a0133928d960e6b93
Addis <pupiatpixar@gmail.com>
2023-01-25 16:05:53 +0300

    Project - Cleaned

--- lib/constants/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color kBackground = Color.fromRGBO(235, 235, 235, 1);
-const Color kPrimaryColor = Color.fromRGBO(239, 11, 11, 1);
-const Color sPrimaryColor = Color.fromRGBO(228, 164, 28, 1);
