a3cb612bb17a91780b211cabaaa8dff5ed0bf923
Addis <pupiatpixar@gmail.com>
2023-01-25 16:25:52 +0300

    Project Re - Initiated

---
+++ android/app/src/main/kotlin/com/example/doro_mart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.doro_mart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/dashboard_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'constants/color_palette.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: ColorPalette().scaffoldBg,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(15.0, 25.0, 15.0, 5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        //todo
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        height: 42.0,
+                        width: 42.0,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('assets/images/model.jpg'),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.circular(12.0)),
+                      ),
+                    ),
+                    Image.asset(
+                      "assets/logos/logo.png",
+                      height: 80,
+                      width: 200,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        //todo
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        height: 42.0,
+                        width: 42.0,
+                        decoration: BoxDecoration(
+                            color: ColorPalette().btnBg,
+                            borderRadius: BorderRadius.circular(12.0)),
+                        child: Icon(
+                          Icons.shopping_cart,
+                          size: 24.0,
+                          color: ColorPalette().btnFill,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 15.0, top: 15.0),
+                width: (MediaQuery.of(context).size.width / 3) * 2 + 25.0,
+                child: Text('Find Products'),
+              )
+            ],
+          ),
+        ));
+  }
+}
