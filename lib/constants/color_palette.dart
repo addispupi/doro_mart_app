7064532d915badd4de512f7960e89871dcb220e6
Addis <pupiatpixar@gmail.com>
2023-01-27 22:33:19 +0300

    Color Codes Constants - Updated

--- lib/constants/color_palette.dart
+++ lib/constants/color_palette.dart
@@ -7,9 +7,11 @@ class ColorPalette {
   final Color textColor = Color(0xFF3d3c3c);
   final Color textInverseColor = Color(0xFFFFFFFF);
   final Color primaryColor = Color(0xFFC68E11);
+  final Color primaryGradientStart = Color(0xFF9e6d03);
+  final Color primaryGradientEnd = Color(0xFF3a2904);
   final Color secondaryColor = Color(0xFFED0C0D);
   final Color btnBg = Color(0xFFE00808);
   final Color btnFill = Color(0xFFf7f4f4);
   final Color btnActive = Color(0xFFFFB30F);
-  final Color inputTextBg = Color(0xFFD3D3D3);
+  final Color inputText = Color(0xFFD3D3D3);
 }
